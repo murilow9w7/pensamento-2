4d8995cbd6b9aa9ad55b43daf1a490bc77de5a24
murilow9w7 <murilo.silva22@escola.pr.gov.br>
2025-05-30 09:39:24 -0300

    Add files via upload

---
+++ operações2.js
@@ -0,0 +1,12 @@
+// contadoe negativo 
+
+let contador = 100;
+contador--;
+
+console.log("O resultado  do decremento é:" + contador );
+
+// interpolação
+
+const nome = "Kayo"
+console.log(`Ola, ${nome}!bem vindo ao nosso sistema.`);
+
